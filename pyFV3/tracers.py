6a46e053f038b36b75a4179fe119adfc9153b93c
Florian Deconinck <deconinck.florian@gmail.com>
2024-12-12 16:19:39 -0500

    Tracer class to handle all things tracers.

---
+++ pyFV3/tracers.py
@@ -0,0 +1,127 @@
+from __future__ import annotations
+from ndsl import Quantity, QuantityFactory
+from ndsl.constants import X_DIM, Y_DIM, Z_DIM
+from typing import Dict, List
+import numpy as np
+
+# FOR REFERENCE - previous descriptive of the tracers, lining up with Pace work
+# tracer_variables = [
+#     "qvapor",
+#     "qliquid",
+#     "qrain",
+#     "qice",
+#     "qsnow",
+#     "qgraupel",
+#     "qo3mr",
+#     "qsgs_tke",
+#     "qcld",
+# ]
+
+
+class Tracers:
+    def __init__(self, factory: QuantityFactory) -> None:
+        self._quantities: Dict[str, Quantity] = {}
+        self._quantity_factory = factory
+        self._dims = [X_DIM, Y_DIM, Z_DIM]
+
+    def copy_tracer_data(
+        self,
+        name: str,
+        data: np.ndarray,
+        units="unknown",
+    ):
+        qty = self._quantity_factory.empty(dims=self._dims, units=units)
+        if data.shape > qty.data.shape:
+            raise ValueError(
+                f"[pyFV3] Tracer {name} size ({data.shape}"
+                f" is bigger than grid {qty.data.shape})"
+            )
+        qty.data[: data.shape[0], : data.shape[1], : data.shape[2]] = data
+        self._quantities[name] = qty
+
+    @property
+    def count(self) -> int:
+        return len(self._quantities)
+
+    def values(self):
+        return self._quantities.values()
+
+    def names(self):
+        return self._quantities.keys()
+
+    def items(self):
+        return self._quantities.items()
+
+    def as_fortran_4D(self) -> np.ndarray:
+        shape = self._quantity_factory.sizer.get_shape(self._dims)
+        var4d = np.empty(
+            (
+                shape[0] - 1,
+                shape[1] - 1,
+                shape[2] - 1,
+                self.count,
+            )
+        )
+        for idx, q in enumerate(self.values()):
+            var4d[:, :, :, idx] = q.data[:-1, :-1, :-1]
+        return var4d
+
+    def as_dict(self) -> Dict[str, Quantity]:
+        return self._quantities
+
+    def __getitem__(self, key):
+        return self._quantities[key]
+
+    def __setitem__(self, key, value):
+        self._quantities[key] = value
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        msg = f"[pyFV3] {self.count} Tracers:\n"
+        for q in self.names():
+            msg += f"  {q}\n"
+        return msg
+
+    @classmethod
+    def make(
+        cls,
+        quantity_factory: QuantityFactory,
+        tracer_mapping: List[str],
+    ):
+        tracers = cls(quantity_factory)
+        for name in tracer_mapping:
+            qty = quantity_factory.empty(dims=tracers._dims, units="kg/m^2")
+            tracers._quantities[name] = qty
+        return tracers
+
+    @classmethod
+    def make_from_fortran(
+        cls,
+        quantity_factory: QuantityFactory,
+        tracer_mapping: List[str],
+        tracer_data: np.ndarray,
+    ) -> Tracers:
+        if len(tracer_data.shape) != 4:
+            raise ValueError("Expected 4D field as input")
+        count = len(tracer_mapping)
+        if count > tracer_data.shape[3]:
+            raise ValueError(
+                f"Mapping size {len(tracer_mapping)} is bigger than"
+                f" data dimensionality {tracer_data.shape[3]}"
+            )
+        tracers = cls(quantity_factory)
+        for idx in range(0, count):
+            tracers.copy_tracer_data(
+                name=tracer_mapping[idx] or f"Tracer_{idx}",
+                data=tracer_data[:, :, :, idx],
+                units="kg/m^2",
+            )
+        return tracers
+
+    @staticmethod
+    def make_mapping(tracer_data: np.ndarray):
+        if len(tracer_data.shape) != 4:
+            raise ValueError("Expected 4D field as input")
+        return tracer_data.shape[3] * [None]
